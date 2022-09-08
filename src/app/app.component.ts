4762db7b852680fd5f449dee908551695305eb3b
eddie <edvin.fajkovic3@gmail.com>
2022-09-08 12:34:09 +0100

    minor update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,8 @@
-import { Component, Input } from "@angular/core";
+import { Component, Input, ElementRef, ViewChild } from "@angular/core";
 import { CustomOption } from "ngx-quill";
-import { FormControl, FormGroup, Validators } from "@angular/forms";
+import { FormControl, FormGroup, Validators, FormBuilder } from "@angular/forms";
+import { HttpClient } from '@angular/common/http';
+import { of } from 'rxjs';
 
 const modules = {
   toolbar: [
@@ -33,14 +35,20 @@ const modules = {
 
 
 export class AppComponent {
-  // editForm: FormGroup;
+  myForm: FormGroup;
+  form: FormGroup;
+  documents: [];
   text: any;
+  title: any;
+  test: string="TESTTEST";
+  allData: any;
+  allDataTitle: string[] = [];;
+  selectedDocument: any;
+  constructor(
+    private http: HttpClient,
+    private formBuilder: FormBuilder,
+    ) {};
 
-  // ngOnInit() {
-  //   this.editForm = new FormGroup({
-  //     text: new FormControl(null)
-  //   });
-  // }
 
   options: CustomOption[] = [
     {
@@ -48,6 +56,55 @@ export class AppComponent {
       whitelist: void 0,
     }
   ];
+
+  checkoutForm = this.formBuilder.group({
+    title: '',
+  });
+  checkoutForm2 = this.formBuilder.group({
+      docID: '',
+  });
+
+  onSubmit(): void {
+    this.title = this.checkoutForm.value.title;
+    console.log(this.title);
+    console.log(this.text.text);
+    this.http.post<any>('http://localhost:1337/', { title: this.title, content: this.text.text }).subscribe(data => {
+        // this.text.text = data.id;
+  })
+  }
+
+  ngOnInit() {
+    this.http.get<any>('http://localhost:1337/').subscribe(data => {
+      this.allData = data;
+      // console.log(this.allData);
+      
+      for (let index = 0; index < this.allData.length; index++) {
+        this.allDataTitle.push(this.allData[index].title);
+      }
+    })
+    console.log(this.allDataTitle);
+            
+  }
+
+  getDocuments() {
+    this.http.get<any>('http://localhost:1337/').subscribe(data => {
+        return data;
+    }) 
+  }
+
+  @ViewChild('document') document!: ElementRef;
+
+	onSelected() {
+		this.selectedDocument = this.document.nativeElement.value;
+    console.log(this.selectedDocument);
+    
+	}
+
+
+  getOneDocument() {
+    console.log("hej");
+    
+  }
 
   onContentChanged = (event) => {
     // console.log(event.html);
@@ -57,6 +114,10 @@ export class AppComponent {
 
   public Save() {
     console.log(this.text.text);
+
+    this.http.post<any>('http://localhost:1337/', { title: this.title, content: this.text.text }).subscribe(data => {
+        // this.text.text = data.id;
+  })
   }
 
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,14 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormBuilder, FormsModule, ReactiveFormsModule } from "@angular/forms";
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
 import { QuillModule } from 'ngx-quill';
 
 @NgModule({
-  imports: [BrowserModule, ReactiveFormsModule, FormsModule, QuillModule.forRoot()],
+  imports: [BrowserModule, ReactiveFormsModule, FormsModule, QuillModule.forRoot(), HttpClientModule],
   declarations: [AppComponent, HelloComponent],
   bootstrap: [AppComponent],
 })
